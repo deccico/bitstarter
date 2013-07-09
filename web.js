8954cde5b32a8d9da13085ac033f7b6b38a053b7
Adrián Deccico <adeccico@atlassian.com>
2013-07-10 00:40:13 +1000

    now reading content from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var buffer = require('buffer');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(new Buffer(fs.readFileSync('index.html'), "utf-8").toString());
 });
 
 var port = process.env.PORT || 5000;
